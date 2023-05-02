ccbd23a492d4fc0512f3070f1798174185c20180
lshangou <lucasmenezesba@gmail.com>
2023-05-02 19:49:57 -0300

    Implementado componente base de busca de destinos

---
+++ src/components/SearchDestinationBox.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-card flat class="q-my-md">
+    <q-form @submit="onSubmit">
+      <q-card-section class="q-py-sm">
+        <div class="text-h6 q-py-none">Reservar Hotel</div>
+      </q-card-section>
+      <q-separator />
+      <q-card-section class="q-pb-none">
+        <label class="text-weight-medium" for="destination">
+          Destino<span class="text-negative">*</span>
+        </label>
+        <q-input
+          outlined
+          dense
+          placeholder="Digite uma cidade..."
+          id="destination"
+          v-model="destination"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Campo vazio.']"
+        />
+      </q-card-section>
+      <q-card-section class="q-pt-none" align="right">
+        <q-btn
+          rounded
+          no-caps
+          color="primary"
+          :label="isResultsPage ? 'Alterar Busca' : 'Buscar'"
+        />
+      </q-card-section>
+    </q-form>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'SearchDestinationBox',
+  props: {
+    isResultsPage: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    return {
+      destination: ref(''),
+      onSubmit: function () {
+        console.log('teste');
+      },
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+label {
+  color: $text-color;
+}
+.q-input {
+  margin-top: 5px;
+}
+</style>
---
+++ src/pages/Hotels.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="row justify-evenly q-pt-lg">
+    <main class="col-9">
+      <div class="col-12">
+        <q-breadcrumbs class="text-primary">
+          <template v-slot:separator>
+            <q-icon size="1.5em" name="chevron_right" color="primary" />
+          </template>
+          <q-breadcrumbs-el label="Início" />
+          <q-breadcrumbs-el label="Hotéis" class="text-black" />
+        </q-breadcrumbs>
+      </div>
+      <div class="col-12">
+        <SearchDestinationBox />
+      </div>
+    </main>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SearchDestinationBox from 'components/SearchDestinationBox.vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { SearchDestinationBox },
+  setup() {
+    return {};
+  },
+});
+</script>
